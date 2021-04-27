e4b9b29b0b2b8cd97acf8e90ea97a149af256286
felix <felix.dittrich@memoresa.de>
2021-04-27 16:29:26 +0200

    update

--- Lightning_learning/T5_summarization.py
+++ Lightning_learning/T5_summarization.py
@@ -1,3 +1,4 @@
+import os
 import json
 import pandas as pd
 import numpy as np
@@ -8,11 +9,11 @@ import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from pytorch_lightning.loggers import TensorBoardLogger
 from sklearn.model_selection import train_test_split
-from termcolor import colored
-import textwrap
 
 from transformers import AdamW, T5ForConditionalGeneration, T5TokenizerFast
 from tqdm.auto import tqdm
+
+os.environ["TOKENIZERS_PARALLELISM"] = "true"
 
 df = pd.read_csv("/home/felix/Desktop/DeepLearning-pyTorch/Lightning_learning/data/news_summary.csv", encoding="latin-1")
 df = df[["text", "ctext"]]
@@ -94,10 +95,18 @@ class NewsSummaryDataModule(pl.LightningDataModule):
 
     def train_dataloader(self):
         return DataLoader(
-            self.train_dataset,
+            self.test_dataset,
             batch_size=self.batch_size,
-            shuffle=True,
-            num_workers=2
+            shuffle=False,
+            num_workers=8
+        )
+        
+    def val_dataloader(self):
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=8
         )
     
     def test_dataloader(self):
@@ -105,7 +114,7 @@ class NewsSummaryDataModule(pl.LightningDataModule):
             self.test_dataset,
             batch_size=self.batch_size,
             shuffle=False,
-            num_workers=2
+            num_workers=8
         )
         
 MODEL_NAME = 't5-base'
@@ -137,14 +146,135 @@ class NewsSummaryModel(pl.LightningModule):
         self.model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME, return_dict=True)
         
     def forward(self, input_ids, attention_mask, decoder_attention_mask, labels=None):
-        output = self.model(input_ids, 
-                            attention_mask=attention_mask,
-                            labels=labels,
-                            decoder_attention_mask=decoder_attention_mask
-                            )
-        
-        return output.loss, output.logits
+        try:
+            loss, logits = self.model(input_ids, 
+                                attention_mask=attention_mask,
+                                labels=labels,
+                                decoder_attention_mask=decoder_attention_mask
+                                )
+            print(type(loss))
+            print(type(logits))
+            return loss, logits
+        except Exception as e:
+            import sys
+            print(str(e))
+            sys.exit(0)
     
     def training_step(self, batch, batch_idx):
-        pass # TODO
+        input_ids = batch["text_input_ids"]
+        attention_mask = batch["text_attention_mask"]
+        labels = batch["labels"]
+        labels_attention_mask = batch["labels_attention_mask"]
+        
+        loss, outputs = self(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            decoder_attention_mask=labels_attention_mask,
+            labels=labels
+            )
+        self.log("train_loss", loss, prog_bar=True, logger=True)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        input_ids = batch["text_input_ids"]
+        attention_mask = batch["text_attention_mask"]
+        labels = batch["labels"]
+        labels_attention_mask = batch["labels_attention_mask"]
+        
+        loss, outputs = self(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            decoder_attention_mask=labels_attention_mask,
+            labels=labels
+            )
+        self.log("val_loss", loss, prog_bar=True, logger=True)
+        return loss
+    
+    def test_step(self, batch, batch_idx):
+        input_ids = batch["text_input_ids"]
+        attention_mask = batch["text_attention_mask"]
+        labels = batch["labels"]
+        labels_attention_mask = batch["labels_attention_mask"]
+        
+        loss, outputs = self(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            decoder_attention_mask=labels_attention_mask,
+            labels=labels
+            )
+        self.log("test_loss", loss, prog_bar=True, logger=True)
+        return loss
+    
+    def configure_optimizers(self):
+        return AdamW(self.parameters(), lr=0.0001)
+    
+model = NewsSummaryModel()
       
+checkpoint_callback = ModelCheckpoint(
+    dirpath="checkpoints",
+    filename="best-checkpoint",
+    save_top_k=1,
+    verbose=True,
+    monitor="val_loss",
+    mode="min"
+)
+
+logger = TensorBoardLogger(save_dir="Lightning_logs", name="news_summary")
+
+early_stopping_callback = EarlyStopping(
+    monitor="val_loss", 
+    min_delta=0.01,
+    patience=2,
+    verbose=True
+)
+
+trainer = pl.Trainer(logger=logger,
+                     callbacks=[checkpoint_callback, early_stopping_callback],
+                     max_epochs=N_EPOCHS,
+                     gpus=0,
+                     progress_bar_refresh_rate=1
+                     )
+
+trainer.fit(model, data_module)
+
+trained_model = NewsSummaryModel.load_from_checkpoint(
+    trainer.checkpoint_callback.best_model_path
+)
+trained_model.freeze()
+
+def summarize(text):
+    text_encoding = tokenizer(
+        text, 
+        max_length=512, 
+        padding="max_length",
+        truncation=True,
+        return_attention_mask=True,
+        add_special_tokens=True,
+        return_tensors="pt"
+    )
+    
+    generated_ids = trained_model.model.generate(
+        input_ids=text_encoding["input_ids"],
+        attention_mask=text_encoding["attention_mask"],
+        max_length=150,
+        num_beams=2,
+        repetition_penalty=2.5,
+        length_penalty=1.0,
+        early_stopping=True
+    )
+    
+    preds = [
+        tokenizer.decode(gen_id, skip_special_tokens=True, clean_up_tokenization_spaces=True)
+        for gen_id in generated_ids
+    ]
+    
+    return "".join(preds)
+
+sample_row = test_df.iloc[0]
+text = sample_row["text"]
+model_summary = summarize(text)
+
+print(model_summary)
+    
+
+    
